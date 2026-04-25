e77af2c15aacb01fe4e310b605f32bac94d09e15
Narendra B T <narendrabt93@gmail.com>
2026-04-25 15:27:17 -0400

    Using locator and css selectors to identify input fields to insert data and fetch alert's error message

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -2,7 +2,7 @@ const {test} = require('@playwright/test')
 const {expect} = require('@playwright/test')
 
 
-test('Browser Playwright Case', async ({browser}) => {
+test.only('Browser Playwright Case', async ({browser}) => {
     
     // Opening browser as new context - where none of the plugins and cookies are available
     const context = await browser.newContext()
@@ -12,19 +12,17 @@ test('Browser Playwright Case', async ({browser}) => {
 
     // assigning URL - https://rahulshettyacademy.com/
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+
+    await page.locator("#username").fill("testUser123")
+    await page.locator("[type='password']").fill("Test@123")
+    await page.locator("#signInBtn").click()
+
+    console.log(await page.locator("[style*='block']").textContent())
 })
 
-test.only('Page Playwright Case', async ({page}) => {
-    
-    // Opening browser as new context - where none of the plugins and cookies are available
-    // const context = await browser.newContext()
-    
-    // Opening new page in the browser
-    // const page = await context.newPage()
-
+test('Page Playwright Case', async ({page}) => {
     // assigning URL
     await page.goto("https://google.com/")
-
     // Method 1 of assertion or expect output
     const title = await page.title()
     console.log(title)
