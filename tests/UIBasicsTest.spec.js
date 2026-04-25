fe71e2ac418fcafb55b713bdf1dcceb6a0c84164
Narendra B T <narendrabt93@gmail.com>
2026-04-25 11:34:51 -0400

    create and programed for basic UI test

---
+++ tests/UIBasicsTest.spec.js
@@ -0,0 +1,26 @@
+const {test} = require('@playwright/test')
+
+
+test('Browser Playwright Case', async ({browser}) => {
+    
+    // Opening browser as new context - where none of the plugins and cookies are available
+    const context = await browser.newContext()
+    
+    // Opening new page in the browser
+    const page = await context.newPage()
+
+    // assigning URL - https://rahulshettyacademy.com/
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+})
+
+test('Page Playwright Case', async ({page}) => {
+    
+    // Opening browser as new context - where none of the plugins and cookies are available
+    // const context = await browser.newContext()
+    
+    // Opening new page in the browser
+    // const page = await context.newPage()
+
+    // assigning URL
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+})
