7d8878fe7115be7758a293613265b7ffa84e4be6
Narendra B T <narendrabt93@gmail.com>
2026-04-25 11:48:37 -0400

    understanding config file and configurations

--- playwright.config.js
+++ playwright.config.js
@@ -1,10 +1,25 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
-
+import { defineConfig, devices, expect } from '@playwright/test';
+import { use } from 'react';
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
   testDir: './tests',
+  
+  // The 2 time outs below are only to over-ride the default time out of 30 seconds for all the components and assertion or expect output
+  // Time out for all the components
+  timeout: 40 * 1000,
+
+  // Expect timeout is exclusively for assertion or expect output timeout
+  expect: {
+    // Expect a condition to be met within 5 seconds.
+    timeout: 50000},
+
+  reporter: 'html',
+
+  use: {
+    browserName: 'chromium'
+  }
 });
 
