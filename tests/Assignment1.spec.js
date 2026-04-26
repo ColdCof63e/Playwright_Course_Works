e5fec759d8ff9f27cff98770f30be1d64cfef026
Narendra B T <narendrabt93@gmail.com>
2026-04-26 12:16:06 -0400

    Method 1 to use one of the explicit waits in playwright

--- tests/Assignment1.spec.js
+++ tests/Assignment1.spec.js
@@ -11,8 +11,14 @@ test('Assignment 1 - Login and fetch 1st product title', async ({page}) => {
     await password.fill(process.env.ASSIGNMENT1_PASSWORD)
     await page.locator("#login").click()
 
+    // Loading time for the page to load after login
+    await page.waitForLoadState('networkidle')
+
     await expect(page).toHaveTitle("Let's Shop")
 
-    console.log(await page.locator(".card-body b").first().textContent())
-    expect(await page.locator(".card-body b").first().textContent()).toBe("ADIDAS ORIGINAL")
+    // console.log(await page.locator(".card-body b").first().textContent())
+    // expect(await page.locator(".card-body b").first().textContent()).toBe("ADIDAS ORIGINAL")
+
+    // To fetch all the product titles and print them in the console
+    console.log(await page.locator(".card-body b").allTextContents())
 })
